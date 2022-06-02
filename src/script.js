6af0fa565e5c132fd02f56cab1cd6eeda596278e
Nikolina <niko94bond@gmail.com>
2022-06-02 18:46:43 +0200

    finnal chages on movie DB

--- src/script.js
+++ src/script.js
@@ -12,13 +12,37 @@ let personalMovieDB = {
 
 };
 
-let a = prompt('óne of the mowies you have watched', ''),
-    b = prompt('how would you rate it?', ''),
-    c = prompt('óne of the mowies you have watched', ''),
-    d = prompt('how would you rate it?', '');
+// let a = prompt('óne of the mowies you have watched', ''),
+//     b = prompt('how would you rate it?', ''),
+//     c = prompt('óne of the mowies you have watched', ''),
+//     d = prompt('how would you rate it?', '');
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++){
+   let  a = prompt('óne of the mowies you have watched', ''),
+    b = prompt('how would you rate it?', '');
+
+        if (a !=null && b != null && a !='' && b !='' && a.length <50){
+            personalMovieDB.movies[a] = b;
+            console.log ("done");
+        } else {
+            console.log('error');
+            i--;
+        }
+    
+}
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10){
+    prompt ('not too many movies watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    prompt ('classy');
+} else if (personalMovieDB.count >= 30){
+    prompt ('reall fun');
+} else {
+    alert('Error');
+}
+
