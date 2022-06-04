8b86cc5de7a226656fb91f2aaffc2ebfc1f68772
Nikolina <niko94bond@gmail.com>
2022-06-04 11:15:22 +0200

    adding calc

--- src/calc.js
+++ src/calc.js
@@ -16,3 +16,26 @@ const anotherNum = ret();
 console.log(anotherNum);
 
 
+
+
+
+
+
+const options = {
+    name: 'test',
+    width: '1024',
+    height: '1024',
+    colors: {
+        border: 'black'
+        
+    }
+}
+
+console.log(options.name);
+
+delete options.name;
+console.log(options);
+
+for (let key in options){
+    console.log (`Свойство ${key} is ${options[key]}`);
+}
